747774caa527cb4d3560a45f4ede06419280ba77
alexbrown <alex.brown577@gmail.com>
2026-02-17 10:44:41 -0500

    Created a tasks file and it is pulling the existing weather reqs

--- base/tasks.py
+++ base/tasks.py
@@ -1,4 +1,6 @@
-from models import db, WeatherRequest
+from base import db
+from base.models import WeatherRequest
+
 
 def process_weather_request(request):
     #api code
@@ -7,8 +9,14 @@ def process_weather_request(request):
 
 def process_pending_weather_requests(app):
     #query db for list of pending requests
+    with app.app_context():
+        raw_list = db.session.execute(
+            db.select(WeatherRequest).filter_by(job_status='Pending').order_by(WeatherRequest.submitted_date.asc())
+        ).scalars()
+        pretty_list = [weather_request.to_dict() for weather_request in raw_list]
+        print(pretty_list)
     #for loop thru reqs
         #execute process_weather_request(request)
     #stop
-
+    #print("this works")
     pass
