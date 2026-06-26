6ac80c14b2cce24bf26e613c95cade671ba94a94
alexbrown <alex.brown577@gmail.com>
2026-06-26 08:25:59 -0400

    Added month lookup

--- base/tasks.py
+++ base/tasks.py
@@ -295,3 +295,18 @@ def get_dates_to_call_for_year(year, month, day):
     for i in date_list:
         date_list_pretty.append(str(i))
     return date_list_pretty
+
+def month_lookup(month):
+    lookup ={1: "January",
+             2: "February",
+             3: "March",
+             4: "April",
+             5: "May",
+             6: "June",
+             7: "July",
+             8: "August",
+             9: "September",
+             10: "October",
+             11: "November",
+             12: "December"}
+    return lookup[month]
